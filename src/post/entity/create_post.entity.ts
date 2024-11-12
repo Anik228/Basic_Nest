b9ca8fe36475dc76220aad8b3e4604defbb90921
Anik Dey <aniklal2020@gmail.com>
2024-11-12 18:01:01 +0600

    one to many & many to one reltion added

--- src/post/entity/create_post.entity.ts
+++ src/post/entity/create_post.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { Column, Entity, PrimaryGeneratedColumn,ManyToOne,JoinColumn } from 'typeorm';
 
 @Entity({ name: 'post', synchronize: true })
 export class PostEntity {
@@ -13,5 +14,9 @@ export class PostEntity {
 
   @Column()
   post_description: string;
+
+  @ManyToOne(() => User, user => user.posts)
+  @JoinColumn({ name: 'country_Id' })
+  user: User;
 
 }
--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { PostEntity } from 'src/post/entity/create_post.entity';
+import { Entity, PrimaryGeneratedColumn, Column,OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
@@ -16,6 +17,9 @@ export class User {
 
   @Column()
   role: string;
+
+  @OneToMany(() => PostEntity, PostEntity => PostEntity.country)
+  posts: PostEntity[];
 }
 
 //nest g controller controller/controller/controller --flat --no-spec
